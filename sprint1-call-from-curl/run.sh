ab142c1ebe1b0e3fa213d7c6a9f86d20c51a087e
Hongliang Li <woodyli1982@gmail.com>
2023-01-12 00:50:11 -0800

    Sprint1: Make run.sh more robust by eliminating hardcode key path.

--- sprint1-call-from-curl/run.sh
+++ sprint1-call-from-curl/run.sh
@@ -1,11 +1,15 @@
-#! /bin/bash
+#!/bin/bash 
 
-mkdir -p out
-cd out
+echo "Looking for text-to-speech API service key file..."
+current_path=$(dirname `readlink -f "$0"`)
+key_path=`readlink -f $current_path/../../keys/text-to-speech-service-key.json`
+echo $key_path
+if ! [ -f $key_path ]; then
+      read -p "Enter the text-to-speech service key file path:" key_path
+fi
 
 echo "Setting GOOGLE_APPLICATION_CREDENTIALS"
-export GOOGLE_APPLICATION_CREDENTIALS=`readlink \
--f ~/startD/gcloud/woodyli1982/keys/*.json`
+export GOOGLE_APPLICATION_CREDENTIALS=$key_path
 
 echo "Installing jq if not"
 if ! dpkg -s "jq" > /dev/null 2>&1; then
@@ -14,21 +18,22 @@ if ! dpkg -s "jq" > /dev/null 2>&1; then
    sudo apt install jq
 fi
 
+mkdir -p out
 echo "Calling text-to-speech API"
 curl -X POST \
 -H "Authorization: Bearer $(gcloud auth application-default \
 print-access-token)" \
 -H "Content-Type: application/json; charset=utf-8" \
--d @../request.json \
+-d @request.json \
 https://texttospeech.googleapis.com/v1/text:synthesize | \
 jq '.audioContent' | \
-sed 's/"//g' > out.base64
+sed 's/"//g' > out/out.base64
 
 echo "Converting result to out.mp3"
-base64 -d out.base64 -d > out.mp3
+base64 -d out/out.base64 -d > out/out.mp3
 
 echo "Playing synthesis voice"
 if [ -f "$(which cmd.exe)" ]; then
-   cmd.exe /C start out.mp3
+   cmd.exe /C start out/out.mp3
 fi
 cd ..
