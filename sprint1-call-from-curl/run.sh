d47388a9e23b5b3bd94e1aa1627cfd480418d865
Hongliang Li <woodyli1982@gmail.com>
2023-01-12 22:34:52 -0800

    Sprint1: Allow user to input a text to be synthesized.

--- sprint1-call-from-curl/run.sh
+++ sprint1-call-from-curl/run.sh
@@ -1,15 +1,21 @@
 #!/bin/bash 
 
+mkdir -p out
+
 echo "Looking for text-to-speech API service key file..."
 current_path=$(dirname `readlink -f "$0"`)
 key_path=`readlink -f $current_path/../../keys/text-to-speech-service-key.json`
 echo $key_path
 if ! [ -f $key_path ]; then
-      read -p "Enter the text-to-speech service key file path:" key_path
+      read -p "Enter the text-to-speech service key file path: " key_path
 fi
 
 echo "Setting GOOGLE_APPLICATION_CREDENTIALS"
 export GOOGLE_APPLICATION_CREDENTIALS=$key_path
+
+read -p "Enter the text to be synthesized: " text
+cp request.json out
+sed "s/TBD/${text}/g" -i out/request.json
 
 echo "Installing jq if not"
 if ! dpkg -s "jq" > /dev/null 2>&1; then
@@ -18,13 +24,12 @@ if ! dpkg -s "jq" > /dev/null 2>&1; then
    sudo apt install jq
 fi
 
-mkdir -p out
 echo "Calling text-to-speech API"
 curl -X POST \
 -H "Authorization: Bearer $(gcloud auth application-default \
 print-access-token)" \
 -H "Content-Type: application/json; charset=utf-8" \
--d @request.json \
+-d @out/request.json \
 https://texttospeech.googleapis.com/v1/text:synthesize | \
 jq '.audioContent' | \
 sed 's/"//g' > out/out.base64
