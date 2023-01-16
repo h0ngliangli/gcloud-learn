cfcf456471450878be448dc857c53d4363b05011
Hongliang Li <woodyli1982@gmail.com>
2023-01-16 15:00:26 -0800

    sprint2: add run.sh with description

---
+++ sprint2-simple-web/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# npm init
+# npm install express
+# create server.js
+# add start script in package.json
+#   "start": node server.js
+# create app.yaml
+# test your app locally by 
+#       npm start
+# deploy the service to google cloud by
+#       gcloud app deploy
+gcloud app 
